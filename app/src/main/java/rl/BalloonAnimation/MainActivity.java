f3aa78b132d8193141e90ddcd9b3a2e015b5162b
ShivaShirsath <Shiva.S.Shirsath@gmail.com>
2021-06-21 16:43:20 +0530

    ‎

---
+++ app/src/main/java/rl/BalloonAnimation/MainActivity.java
@@ -0,0 +1,53 @@
+package rl.BalloonAnimation;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.LinearLayout;
+import android.view.View;
+import android.view.animation.TranslateAnimation;
+import android.widget.Button;
+import android.widget.FrameLayout;
+
+public class MainActivity extends Activity {
+
+	FrameLayout layout;
+    View balloon;
+    boolean isT=false;
+	int start = 0, end = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+		layout = findViewById(R.id.layout);	
+        balloon = findViewById(R.id.car);
+    }
+    public void onSlide(View view) {
+		if (isT) {
+			start = layout.getHeight();
+			end = 0;
+		} else {
+			start = 0;
+			end = layout.getHeight() - balloon.getHeight();
+		}
+
+		TranslateAnimation animate = new TranslateAnimation(
+			0     , // fromXDelta
+			0     , // toXDelta
+			- start , // fromYDelta
+			- end     // toYDelta
+		);
+		balloon.startAnimation(animate);
+		animate.setDuration(500);
+		animate.setFillAfter(true);
+		((Button) view).setText(
+			isT ?
+			"Up"
+			:
+			"Down"
+		);
+		isT = ! isT;
+    }
+}
